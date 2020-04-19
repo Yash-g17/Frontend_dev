fd1d225441f1b4d2ddb17dbe83b7dabd6d0f4a6d
Yash-g17 <Yash2520175@gmail.com>
2020-04-19 16:13:16 +0530

    text boxes increase size when selected

---
+++ src/javascript/contact-us-script.js
@@ -0,0 +1,7 @@
+function myfunction() {
+    var x = document.activeElement.id;
+    console.log(x);
+    document.getElementById(document.activeElement.id).style.transform = 'scale(1.05)';
+    document.getElementById(document.)
+}
+document.querySelector('body').addEventListener('click', myfunction);
