1cb1183f3a979899ba20cb7ec56a477d087c6195
Yash-g17 <Yash2520175@gmail.com>
2020-04-21 23:24:05 +0530

    home page updated

--- src/javascript/home-script.js
+++ src/javascript/home-script.js
@@ -14,17 +14,17 @@ $('#left-button').click(function() {
 // var parent = document.getElementById('scroll-cards');
 // var child = document.getElementsByClassName('card');
 // child.style.paddingBottom = child.offsetWidth - child.clientWidth + "px";
-const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-var y = document.getElementById('first-row-container').offsetWidth;
+// const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+// const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+// var y = document.getElementById('first-row-container').offsetWidth;
 
-function scalingFunction() {
-    console.log("y=" + y);
-    x = ((body.innerWidth) / (y));
-    console.log("x=" + x);
-    console.log("inner width" + window.innerWidth);
-    document.getElementById('first-row-container').style.transform = ("scale(" + x + ")");
-    console.log(document.getElementById('first-row-container').offsetWidth);
-    // document.getElementById('first-row-container').style.transform
-}
-window.addEventListener('resize', scalingFunction);
+// function scalingFunction() {
+//     console.log("y=" + y);
+//     x = ((body.innerWidth) / (y));
+//     console.log("x=" + x);
+//     console.log("inner width" + window.innerWidth);
+//     document.getElementById('first-row-container').style.transform = ("scale(" + x + ")");
+//     console.log(document.getElementById('first-row-container').offsetWidth);
+//     // document.getElementById('first-row-container').style.transform
+// }
+// window.addEventListener('resize', scalingFunction);
